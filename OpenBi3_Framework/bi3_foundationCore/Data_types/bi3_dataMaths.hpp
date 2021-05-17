cd88e30beed3ce29fce2ac66d496b9e46b385040
I.C. Juan Torres <jctorresce@outlook.com>
2021-05-16 23:48:43 -0500

    Updated from macbook (16/5/21 23:49)

---
+++ OpenBi3_Framework/bi3_foundationCore/Data_types/bi3_dataMaths.hpp
@@ -0,0 +1,60 @@
+/*
+ ===============================================================================
+ ·File:       bi3_dataMaths.hpp
+ ·Project:    OpenBi3 framework technology
+ 
+ ·Author:     Juan Torres on 12/05/21
+ ·Group:      CTDi Pro. Copyright© 2021
+ 
+ Any use, distribution or reproduction of this material (in part or as a whole),
+ done by any entity or group of entities, that is against specified terms in the
+ license or without appropriate authorization,  will be sanctioned in conformity
+ with any and all applicable laws. Original/source work is published by CTDi Pro
+ under the Creative Commons  Attribution-ShareAlike (4.0) International License.
+ Unless required by the applicable law or agreed to in writing, this software is
+ distributed on an  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. Visit http://creativecommons.org/licenses/by/4.0/ to
+ review the contents and specific language governing permissions and limitations
+ under the License or write to Creative Commons at PO Box 1866 in Mountain View,
+ CA 94042, United States (USA).
+ 
+ * DISCLAIMER *
+ - This is all preliminary work or (pre-alpha). The author takes no responsibility
+ for errors present in the code or their effects on other software, due to the
+ inclusion or implementation without proper adaptation. However, any report or
+ feedback about such errors would be greatly appreciated.
+ 
+ * ACKNOWLEDGEMENT *
+ - This work is created to participate in the Hilti Smart Construction Challenge
+ contest promoted by Hilti and hosted by the TAIKAI organization. All information
+ about the HSCC is found at https://taikai.network/en/hilti/challenges/hiltiscc/.
+ 
+ ===============================================================================
+ */
+
+#ifndef bi3_dataMaths_hpp
+#define bi3_dataMaths_hpp
+
+#include <stdio.h>
+#include <bi3_dataNumbers.hpp>
+
+#endif /* bi3_dataMaths_hpp */
+
+
+//  ==============================================================================
+//      DATA TYPE DEFINITIONS FOR MATHEMATICAL DATA & OBJECTS
+//  ==============================================================================
+
+//  _____ BASIC MATH COMPONENTS. _________________________________________________
+//  ______________________________________________________________________________
+struct bi3MathTerm {
+    <#struct fields#>
+};
+
+struct bi3MathExpr {
+    <#struct fields#>
+};
+
+struct bi3MathFunc {
+    <#struct fields#>
+};
---
+++ OpenBi3_Framework/bi3_foundationCore/Data_types/bi3_dataNumbers.hpp
@@ -0,0 +1,149 @@
+/*
+ ===============================================================================
+ ·File:       bi3_dataNumbers.hpp
+ ·Project:    OpenBi3 framework technology
+ 
+ ·Author:     Juan Torres on 12/05/21
+ ·Group:      CTDi Pro. Copyright© 2021
+ 
+ Any use, distribution or reproduction of this material (in part or as a whole),
+ done by any entity or group of entities, that is against specified terms in the
+ license or without appropriate authorization,  will be sanctioned in conformity
+ with any and all applicable laws. Original/source work is published by CTDi Pro
+ under the Creative Commons  Attribution-ShareAlike (4.0) International License.
+ Unless required by the applicable law or agreed to in writing, this software is
+ distributed on an  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. Visit http://creativecommons.org/licenses/by/4.0/ to
+ review the contents and specific language governing permissions and limitations
+ under the License or write to Creative Commons at PO Box 1866 in Mountain View,
+ CA 94042, United States (USA).
+ 
+ * DISCLAIMER *
+ - This is all preliminary work or (pre-alpha). The author takes no responsibility
+ for errors present in the code or their effects on other software, due to the
+ inclusion or implementation without proper adaptation. However, any report or
+ feedback about such errors would be greatly appreciated.
+ 
+ * ACKNOWLEDGEMENT *
+ - This work is created to participate in the Hilti Smart Construction Challenge
+ contest promoted by Hilti and hosted by the TAIKAI organization. All information
+ about the HSCC is found at https://taikai.network/en/hilti/challenges/hiltiscc/.
+ 
+ ===============================================================================
+ */
+
+#ifndef bi3_dataNumbers_hpp
+#define bi3_dataNumbers_hpp
+
+#include <stdio.h>
+
+#endif /* bi3_dataNumbers_hpp */
+
+
+//  ==============================================================================
+//      DATA TYPE DEFINITIONS FOR NUMERIC VARIABLES OR DATA
+//  ==============================================================================
+
+//  _____ NATURAL NUMBERS ( 0 ≥ VAR ≥ +N) ________________________________________
+//  ______________________________________________________________________________
+struct bi3N8 {
+    <#struct fields#>
+};
+
+struct bi3N32 {
+    <#struct fields#>
+};
+
+struct bi3N64 {
+    <#struct fields#>
+};
+
+//  _____ INTEGER NUMBERS ( -Z ≥ VAR ≥ +Z ) ______________________________________
+//  ______________________________________________________________________________
+struct bi3Z8 {
+    <#struct fields#>
+};
+
+struct bi3Z32 {
+    <#struct fields#>
+};
+
+struct bi3Z64 {
+    <#struct fields#>
+};
+
+//  _____ RATIONAL NUMBERS ( -Q ≥ VAR ≥ +Q ) _____________________________________
+//  ______________________________________________________________________________
+struct bi3Q8 {
+    <#struct fields#>
+};
+
+struct bi3Q32 {
+    <#struct fields#>
+};
+
+struct bi3Q64 {
+    <#struct fields#>
+};
+
+//  _____ IRRATIONAL NUMBERS ( -I ≥ VAR ≥ +I ) ___________________________________
+//  ______________________________________________________________________________
+struct bi3I8 {
+    <#struct fields#>
+};
+
+struct bi3I32 {
+    <#struct fields#>
+};
+
+struct bi3I64 {
+    <#struct fields#>
+};
+
+//  _____ DIRECT NUMBERS ( -R ≥ VAR ≥ +R ) also known as Real numbers ____________
+//  ______________________________________________________________________________
+struct bi3R8 {
+    <#struct fields#>
+};
+
+struct bi3R32 {
+    <#struct fields#>
+};
+
+struct bi3R64 {
+    <#struct fields#>
+};
+
+//  _____ LATERAL NUMBERS ( -L ≥ VAR ≥ +L ) also known as Imaginary numbers ______
+//  ______________________________________________________________________________
+struct bi3L8 {
+    <#struct fields#>
+};
+
+struct bi3L32 {
+    <#struct fields#>
+};
+
+struct bi3L64 {
+    <#struct fields#>
+};
+
+//  _____ COMPLEX NUMBERS ( -C ≥ VAR ≥ +C ) ______________________________________
+//  ______________________________________________________________________________
+struct bi3C8 {
+    <#struct fields#>
+};
+
+struct bi3C32 {
+    <#struct fields#>
+};
+
+struct bi3C64 {
+    <#struct fields#>
+};
+
+//  _____ TERNARY-base27 ENCODED NATURAL NUMBER ( 0 ≥ VAR ≥ +4·(27^5) ) __________
+//  ______________________________________________________________________________
+struct bi3TEN {
+    <#struct fields#>
+};

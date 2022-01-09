9d95d749f273f2013102b85a8a7dcf30336e4c0b
juanKme <jctorresce@outlook.com>
2022-01-08 20:04:05 -0500

    Retomando este proyecto.

--- OpenBi3_Framework/main.cpp
+++ OpenBi3_Framework/main.cpp
@@ -35,6 +35,7 @@
 // External references and libraries
 #include <algorithm>
 //#include <assert.h>
+#include <cstdlib>
 //#include <chrono>
 //#include <complex.h>
 //#include <ctype.h>
@@ -44,9 +45,9 @@
 #include <iostream>
 //#include <inttypes.h>
 //#include <iso646.h>
-//#include <limits.h>
+#include <limits.h>
 //#include <locale.h>
-//#include <math.h>
+#include <math.h>
 //#include <setjmp.h>
 //#include <signal.h>
 //#include <stdalign.h>
@@ -67,43 +68,109 @@
 //#include <wchar.h>
 //#include <wctype.h>
 
+#include <unistd.h>
+#include <term.h>
+
 // Internal references
 #include "bi3_framework.hpp"
 #include "bi3_foundationCore/Admin_tools/bi3_ErrorHandling.hpp"
+
+/*void cleanConsole(){
+    if (!cur_term)
+    {
+        int result;
+        setupterm( NULL, STDOUT_FILENO, &result );
+        if (result <= 0) return;
+    }
+    
+    putp( tigetstr( "clear" ) );
+}*/
+// function for registering data to the console
 template<typename inputType>
 void logtoConsole(inputType message, bool endLine) {
     std::cout << message;
     if (endLine == true) {std::cout << std::endl;}
 }
-
+// function for registering data to the booklog file
 template<typename inputType>
 void logtoFile(inputType message,
-               inputType logTime,
-               inputType *logFilePointer) {
-    std::string fullLog = " · Booklog record : "+logTime+" — "+message+"\n";
-    fprintf(logFilePointer = fopen("Framework_Booklog.txt", "a+"), "%s", fullLog);
+               inputType logTimeID,
+               inputType logFileLocation,
+               inputType logFilePointer) {
+    //FILE *logFilePointer;
+    //if ((logFilePointer = fopen("~/Downloads/Framework_Booklog.txt", "W+")) == NULL) {
+        //logtoConsole("Booklog file cannot be opened. The program will now create a new file.", true);
+    //}
+    std::string fullLog = " · ID# %s"+logTimeID+" — %s"+message+"\n ";
+    logtoConsole("Writing to file: %s"+logFileLocation+"___>>\n"+fullLog,true);
+    fprintf(logFilePointer = fopen(logFileLocation,"a+"),"%s",static_cast<void>(fullLog));
     fclose(logFilePointer);
 }
-
-int main(int argc, const char * argv[]) {
-    // Setting up the working environment for the framework interac
-    bool terminationFlag = false;
-    time_t logTime;
-    FILE *logFilePointer;
-    char sentence[1000]; // not really necessary
+// function for conforming a log's ID# based on the current system time
+char* timetoLogID(clock_t initExecutionTime){
+    time_t timer = time(NULL); // get current time; same as: time(&timer);*/
+    clock_t logTime;
+    struct tm y2k = {0}; y2k.tm_hour = 0; y2k.tm_min = 0; y2k.tm_sec = 0; y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
     
-    struct tm y2k = {0};
     double seconds;
-    y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
-    y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
-    time(&logTime);  // get current time; same as: timer = time(NULL)
+    seconds = difftime(timer,mktime(&y2k));
     
-    if ((logFilePointer = fopen("Framework_Booklog.txt", "a+")) == NULL) {
-        logtoConsole("Booklog file cannot be opened. The program will now create a new file.", true);
-        logFilePointer = fopen("Framework_Log.txt", "a");
+    //time(&logTime);  // get current time; same as: timer = time(NULL)
+    logTime = clock() - initExecutionTime;
+    const char sentence[] = "ID# %lu:"+&logTime;//&((float)logTime)/CLOCKS_PER_SEC);
+    
+    std::string s = std::to_string(number);
+    char const *pchar = s.c_str();  //use char const* as target type
+    
+    // ejemplo de conversión de int a char
+#include <iostream>
+#include <charconv> // from_chars, to_chars
+#include <string>
+    
+    int main() {
+        std::string str { "xxxxxxxx" };
+        const int value = 1986;
+        
+        const auto res = std::to_chars(str.data(),
+                                       str.data() + str.size(),
+                                       value);
+        
+        if (res.ec == std::errc())    {
+            std::cout << str << ", filled: "
+            << res.ptr - str.data() << " characters\n";
+        }
+        else if (res.ec == std::errc::value_too_large) {
+            std::cout << "value too large!\n";
+        }
     }
     
-    //logtoFile("Testing", logTime, *logFilePointer);
+    // ejemplo de conversión de float a char
+    std::string str{ "xxxxxxxxxxxxxxx" }; // 15 chars for float
+    const auto res = std::to_chars(str.data(), str.data() + str.size(),  value);
+    if (res.ec == std::errc())     {
+        std::cout << str << ", filled: "
+        << res.ptr - str.data() << " characters\n";
+    }
+    else if (res.ec == std::errc::value_too_large)     {
+        std::cout << "value too large!\n";
+    }
+    
+    
+    return sentence;
+}
+// main loop for (demo) managing data using the Bi3 Framework
+int main(int argc, const char * argv[]) {
+    // Setting up the working environment for this demo
+    bool terminationFlag = false;
+    clock_t initialExecutionTime = clock();
+    const char logFileLocation[] = "Framework_Logbook.txt"; //validate appropiate support location
+    FILE *logFilePointer ;
+    if ((logFilePointer = fopen(logFileLocation, "r+")) == NULL) {
+        logtoConsole("Logbook file cannot be opened. The program will now create a new file.", true);
+        char *logID = timetoLogID(initialExecutionTime);
+        logtoConsole(logID, true);
+        //logtoFile("New execution log.", "1", logFileLocation, logFilePointer);
+    }
     fclose(logFilePointer);
     
     do {
@@ -117,47 +184,57 @@ int main(int argc, const char * argv[]) {
             
             do {
                 // B. hacer continuamente un switch hasta que el usuario elija salir (X)
+                //cleanConsole();
+                std::cout << initialExecutionTime << std::endl;
                 logtoConsole(salute, true);
+                std::cout << "Ingrese una opción para el programa (1 ~ 8)";
+                std::cin >> control;
 
                 switch (control) {
                     case '1':
                         // 1. crear un nuevo modelo desde objeto vacio tipo "BIM_model"
-                        //<#statements#>
+                        std::cout << "Creando nuevo modelo...";
                         break;
                  
                     case '2':
                         // 2. cargar un modelo BIM desde contenido en archivo (si existe)
-                        //<#statements#>
+                        std::cout << "Cargando modelo de archivo...";
                         break;
                  
                     case '3':
                         // 3. intercambiar de modelo activo en la sesión de trabajo
-                        //<#statements#>
+                        std::cout << "Intercambiando el modelo activo...";
                         break;
                  
                     case '4':
                         // 4. editar el modelo BIM activo en la sesión de trabajo (múltiples submenus)
-                        //<#statements#>
+                        std::cout << "Editando el modelo activo...";
                         break;
                  
                     case '5':
                         // 5. guardar en archivo el estado del modelo activo en la sesión de trabajo
-                        //<#statements#>
+                        std::cout << "Guardando en archivo el modelo activo...";
                         break;
                  
                     case '6':
-                        // 6. terminar la sesión de trabajo
-                        //<#statements#>
+                        // 6. preparar & revisar informes de la información en el modelo activo
+                        std::cout << "Preparando informes del modelo activo...";
                         break;
+                    case '7':
+                        // 7. Realizar ajustes a la configuración del programa
+                        std::cout << "Ajustando la configuración del programa...";
+                        break;
+                    case '8':
+                        // 8. terminar la sesión de trabajo
+                        std::cout << "Cerrarndo sesión...";
+                        control='e';
+                        break;
+                        
                  
                     default:
-                        // — por defecto el programa permite ajustar la configuración del programa
-                        char oper;
-                        float num1, num2;
-                        std::cout << "Enter an operator (+, -, *, /): ";
-                        std::cin >> oper;
-                        std::cout << "Enter two numbers: " << std::endl;
-                        std::cin >> num1 >> num2;
+                        // — el programa calcula los parámetros relevantes del modelo activo
+                        std::cout << "Calculando los parámetros relevantes del modelo...";
+                        
                         break;
                 }
                 
@@ -169,6 +246,7 @@ int main(int argc, const char * argv[]) {
                 
                 logtoConsole(" + ", true);control = std::cin.get();logtoConsole("/cl", true);
                 cycle += 1;
+                std::cout << cycle;
             } while ((control != 'e'));
             // if error message throw "exception"; // Throw an exception when a problem arise
             // if error code throw 12345;
@@ -177,20 +255,20 @@ int main(int argc, const char * argv[]) {
         }
         catch (const bi3_ErrorObj errorObject) {
             // Block of code to handle & report errors during main routine/loop
-            std::cerr << "Exception #" << "errorObject.errorCode" << " : " << std::endl;
+            //logtoFile("Exception #errorObject.errorCode : \n", <#inputType logTimeID#>, <#inputType *logFilePointer#>)
             std::cerr << "errorObject.errorDescription" << std::endl;
         }
         catch (std::exception stdException){
             // Block of code to handle & report errors during main routine/loop
-            std::cerr << "Exception:" << stdException.what() << std::endl;
+            logtoConsole(stdException.what(), true);
         }
         catch (const int errorCode) {
             // Block of code to handle & report errors during main routine/loop
-            std::cerr << "Exception #" << errorCode << std::endl;
+            logtoConsole(errorCode, true);
         }
         catch (const char* errorMessage) {
             // Block of code to handle & report errors during main routine/loop
-            std::cerr << "Exception: " << errorMessage << std::endl;
+            logtoConsole(errorMessage, true);
         }
     } while (terminationFlag == false);
     
